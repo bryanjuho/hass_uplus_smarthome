7f8bb73c0925c06429cdb9a94e099571d4bc60f1
홍주호 <juho.hong@datamaker.io>
2022-11-26 02:59:46 +0900

    move hacs.json

--- custom_components/hass_uplus_smarthome/light.py
+++ custom_components/hass_uplus_smarthome/light.py
@@ -5,7 +5,7 @@ import logging
 from typing import Any
 
 import requests
-from pprint import pformat
+from pprint import pformat, pprint
 import voluptuous as vol
 from homeassistant.const import CONF_NAME
 from homeassistant.core import HomeAssistant, callback
@@ -42,11 +42,6 @@ def setup_platform(
 
 
 class UplusMoodLight(LightEntity):
-    @callback
-    def async_update_callback(self):
-        """Update the device's state."""
-        self.async_write_ha_state()
-
     def turn_on(self, **kwargs: Any) -> None:
         pass
 
@@ -84,11 +79,9 @@ class UplusMoodLight(LightEntity):
         await self.api.light_turn_off()
 
     async def async_update(self):
-
         session = async_get_clientsession(self.hass)
-
         url = 'https://hmiotsas.uplus.co.kr:7002/smartlight/ws/smartlight/common/getDeviceStatusThings'
-        header = {
+        headers = {
             'Host': 'hmiotsas.uplus.co.kr:7002',
             'X-HIT-Version': 'v3',
             'Connection': 'keep-alive',
@@ -97,37 +90,34 @@ class UplusMoodLight(LightEntity):
             'X-HIT-Log-Key': f'{datetime_string()}.{random_token(upper=True, k=4)}.SLGT',
             'Accept-Language': 'en-US,en;q=0.9',
             'Origin': 'https://hmiotsas.uplus.co.kr:7002',
-            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_1_1 like Mac OS X) '
-                          'AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
-            'Referer': 'https://hmiotsas.uplus.co.kr:7002/smartlight/smartlight/main.html'
-                       '?mid=LC100&uuid=93BAB3FB-292B-34AC-93E1-70DDD599181C&pmid=HS001&vendor=0000584&model=LT-Z24C01',
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+            'Referer': 'https://hmiotsas.uplus.co.kr:7002/smartlight/smartlight/main.html?mid=LC100&uuid=93BAB3FB-292B-34AC-93E1-70DDD599181C&pmid=HS001&vendor=0000584&model=LT-Z24C01',
             'X-HIT-SvcCode': '9119',
             'Content-Length': '411',
             'X-HIT-Channel': 'APP-SMARTLIGHT',
             'X-HIT-Vendor': '0000584',
             'Content-Type': 'application/json'
         }
-        data = {"account": {"one_id": "sallyhaeri@hanmail.net", "one_id_key": "4003915545",
-                            "sso_key": "72aa86051a9068a26fde078994a3bd18",
-                            "session_key": "H000101_20C6D637-9B3F-4E57-A092-3F9A4509BBC6_H585LNPS1RMXEOAOAD9M"},
-                "home": {"home_code": "500123840867"},
-                "device": {"uuid": "93BAB3FB-292B-34AC-93E1-70DDD599181C", "model": "LT-Z24C01"}, "mid": "LC100",
-                "parameter": {"agsset": {"cmd_request": {"cmd_id": 2, "parameters": [{"filter": "ALL"}]}}}}
+        data = {'account': {'one_id': 'sallyhaeri@hanmail.net', 'one_id_key': '4003915545',
+                            'sso_key': '72aa86051a9068a26fde078994a3bd18',
+                            'session_key': 'H000101_20C6D637-9B3F-4E57-A092-3F9A4509BBC6_H585LNPS1RMXEOAOAD9M'},
+                'home': {'home_code': '500123840867'},
+                'device': {'uuid': '93BAB3FB-292B-34AC-93E1-70DDD599181C', 'model': 'LT-Z24C01'}, 'mid': 'LC100',
+                'parameter': {'agsset': {'cmd_request': {'cmd_id': 2, 'parameters': [{'filter': 'ALL'}]}}}}
 
-        response = await session.post(
-            url=url,
-            headers=header,
-            data=json.dumps(data),
-            timeout=10
-        )
-        response.raise_for_status()
-        result = await response.json()
-        statuses = result['parameter']['cmd_report']['parameters']
+        async with session.post(
+                url=url,
+                headers=headers,
+                json=data,
+                timeout=10
+        ) as response:
+            result = await response.json()
+            response.raise_for_status()
+            statuses = result['parameter']['cmd_report']['parameters']
 
-        params = ['device_state', 'brightness', 'color']
-        status_to_dict = {k: v for status in statuses for k, v in status.items() if k in params}
-
-        self.api.light.is_on = bool(status_to_dict['device_state'])
-        self.api.light.brightness = status_to_dict['brightness']
-        self._state = self.api.light.is_on
-        self._brightness = self.api.light.brightness
+            params = ['device_status', 'brightness', 'color']
+            status_to_dict = {k: v for status in statuses for k, v in status.items() if k in params}
+            self.api.light.is_on = bool(int(status_to_dict['device_status']))
+            self.api.light.brightness = int(status_to_dict['brightness']) * 2.55
+            self._state = self.api.light.is_on
+            self._brightness = self.api.light.brightness
--- custom_components/hass_uplus_smarthome/test.py
+++ custom_components/hass_uplus_smarthome/test.py
@@ -7,7 +7,7 @@ from custom_components.hass_uplus_smarthome.utils import datetime_string, random
 
 if __name__ == '__main__':
     url = 'https://hmiotsas.uplus.co.kr:7002/smartlight/ws/smartlight/common/getDeviceStatusThings'
-    header = {
+    headers = {
         'Host': 'hmiotsas.uplus.co.kr:7002',
         'X-HIT-Version': 'v3',
         'Connection': 'keep-alive',
@@ -16,26 +16,24 @@ if __name__ == '__main__':
         'X-HIT-Log-Key': f'{datetime_string()}.{random_token(upper=True, k=4)}.SLGT',
         'Accept-Language': 'en-US,en;q=0.9',
         'Origin': 'https://hmiotsas.uplus.co.kr:7002',
-        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_1_1 like Mac OS X) '
-                      'AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
-        'Referer': 'https://hmiotsas.uplus.co.kr:7002/smartlight/smartlight/main.html'
-                   '?mid=LC100&uuid=93BAB3FB-292B-34AC-93E1-70DDD599181C&pmid=HS001&vendor=0000584&model=LT-Z24C01',
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+        'Referer': 'https://hmiotsas.uplus.co.kr:7002/smartlight/smartlight/main.html?mid=LC100&uuid=93BAB3FB-292B-34AC-93E1-70DDD599181C&pmid=HS001&vendor=0000584&model=LT-Z24C01',
         'X-HIT-SvcCode': '9119',
         'Content-Length': '411',
         'X-HIT-Channel': 'APP-SMARTLIGHT',
         'X-HIT-Vendor': '0000584',
         'Content-Type': 'application/json'
     }
-    data = {"account": {"one_id": "sallyhaeri@hanmail.net", "one_id_key": "4003915545",
-                        "sso_key": "72aa86051a9068a26fde078994a3bd18",
-                        "session_key": "H000101_20C6D637-9B3F-4E57-A092-3F9A4509BBC6_H585LNPS1RMXEOAOAD9M"},
-            "home": {"home_code": "500123840867"},
-            "device": {"uuid": "93BAB3FB-292B-34AC-93E1-70DDD599181C", "model": "LT-Z24C01"}, "mid": "LC100",
-            "parameter": {"agsset": {"cmd_request": {"cmd_id": 2, "parameters": [{"filter": "ALL"}]}}}}
+    data = {'account': {'one_id': 'sallyhaeri@hanmail.net', 'one_id_key': '4003915545',
+                        'sso_key': '72aa86051a9068a26fde078994a3bd18',
+                        'session_key': 'H000101_20C6D637-9B3F-4E57-A092-3F9A4509BBC6_H585LNPS1RMXEOAOAD9M'},
+            'home': {'home_code': '500123840867'},
+            'device': {'uuid': '93BAB3FB-292B-34AC-93E1-70DDD599181C', 'model': 'LT-Z24C01'}, 'mid': 'LC100',
+            'parameter': {'agsset': {'cmd_request': {'cmd_id': 2, 'parameters': [{'filter': 'ALL'}]}}}}
     response = requests.post(
         url=url,
-        headers=header,
-        data=json.dumps(data),
-        timeout=10
+        headers=headers,
+        # data=json.dumps(data),
+        timeout=2
     )
     pprint(response.json())
